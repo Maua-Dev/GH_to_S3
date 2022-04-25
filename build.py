7b836e3c17fed277ba6d7574815876e16946cd0b
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-04-24 21:58:56 -0300

    Update build.py

--- build.py
+++ build.py
@@ -2,12 +2,6 @@ import os
 import shutil
 
 def build():
-    #create a venv
-    #install requirements
-    #create dir upload
-    #copy gh_to_s3 to venv\lib\site-packages
-    #zip venv\lib\site-packages to upload\function.zip
-    #return "upload"
 
     #create venv
     os.system("python3 -m venv venv")
@@ -22,10 +16,9 @@ def build():
     os.system("mkdir upload")
 
     #copy gh_to_s3 to venv\lib\site-packages
-    os.system("cp gh_to_s3.py venv/lib/python3.6/site-packages/")
+    os.system("cp lambda_function\gh_to_s3.py venv\lib\site-packages")
 
     #zip venv\lib\site-packages to upload\function.zip
-    shutil.make_archive("upload/function", "zip", "venv/lib/python3.6/site-packages") 
+    shutil.make_archive("upload/function", "zip", "venv/lib/site-packages") 
 
     return "upload"
-
