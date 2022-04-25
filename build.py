07f545393d053b0bb1a332d922a6aeaed46d3220
Bruno Vilardi Bueno <62917140+Brvilardi@users.noreply.github.com>
2022-04-25 16:49:34 -0300

    one line cmd

--- build.py
+++ build.py
@@ -4,19 +4,19 @@ import shutil
 def build():
 
     #create venv
-    os.system("python -m venv venv")
+    os.system("python -m venv venv; source venv/bin/activate; pip install -r lambda_function/requirements.txt; mkdir upload ; cp lambda_function\gh_to_s3.py venv\lib\site-packages")
 
-    #activate venv
-    os.system("source venv/bin/activate")
+#     #activate venv
+#     os.system("")
 
-    #install requirements
-    os.system("pip install -r lambda_function/requirements.txt")
+#     #install requirements
+#     os.system("")
 
-    #create dir upload
-    os.system("mkdir upload")
+#     #create dir upload
+#     os.system("")
 
-    #copy gh_to_s3 to venv\lib\site-packages
-    os.system("cp lambda_function\gh_to_s3.py venv\lib\site-packages")
+#     #copy gh_to_s3 to venv\lib\site-packages
+#     os.system("")
 
     #zip venv\lib\site-packages to upload\function.zip
     shutil.make_archive("upload/function", "zip", "venv/lib/site-packages") 
