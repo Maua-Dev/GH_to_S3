c7cc5024476be27ad576ead7197f3a7b182d7c4c
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-04-26 09:04:57 -0300

    Update build.py

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ import shutil
 def build():
 
     #create venv
-    os.system("python -m venv venv; source venv/bin/activate; pip install -r lambda_function/requirements.txt; mkdir upload ; cd venv ; ls ; cp lambda_function\gh_to_s3.py venv\lib\Python3.8\site-packages")
+    os.system("python -m venv venv; source venv/bin/activate; pip install -r lambda_function/requirements.txt; mkdir upload ; cd venv ; cd lib; Python3.8 ; ls ; cp lambda_function\gh_to_s3.py venv\lib\Python3.8\site-packages")
 
 #     #activate venv
 #     os.system("")
