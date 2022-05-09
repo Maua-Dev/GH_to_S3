18ca3f4f60a5202d1c2a0aa8dc9189751c4020e4
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-05-09 20:30:12 -0300

    Update build.py

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ def build():
 
     create_dir_upload = "mkdir upload;"
 
-    copy_to_site_packages = "cp lambda_function\gh_to_s3.py venv\lib\python3.8\site-packages;"
+    copy_to_site_packages = "cp lambda_function/gh_to_s3.py venv/lib/python3.8/site-packages;"
 
     os.system(f"{create_venv} {activate_venv} {install_requirements} {create_dir_upload} {copy_to_site_packages}")
 
