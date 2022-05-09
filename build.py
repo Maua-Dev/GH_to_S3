6883b415c8118d30a4d2670956af4b29f5a8e610
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-05-09 20:25:39 -0300

    Update build.py

--- build.py
+++ build.py
@@ -7,12 +7,12 @@ def build():
 
     activate_venv= "source venv/bin/activate;"
 
-    install_requirements= "pip install -r requirements.txt;"
+    install_requirements= "pip install -r lambda_function/requirements.txt;"
 
     create_dir_upload = "mkdir upload;"
 
     copy_to_site_packages = "cp lambda_function\gh_to_s3.py venv\lib\python3.8\site-packages;"
-    
+
     os.system(f"{create_venv} {activate_venv} {install_requirements} {create_dir_upload} {copy_to_site_packages}")
 
     #zip venv\lib\site-packages to upload\function.zip
